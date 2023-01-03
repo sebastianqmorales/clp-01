54be436a4c84b2858794cedfbc42f67344199a81
Sebastian <swebermorales@gmail.com>
2023-01-03 13:49:34 -0800

    inital commit

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte'
+  import Logo from './CuatexLogo.svg';
+</script>
+
+<Navbar let:hidden let:toggle>
+    <NavBrand href="/">
+      <img
+        src={Logo}
+        class="mr-3 h-6 sm:h-14"
+        alt="Cuatex Logo"
+      />
+    </NavBrand>
+    <NavHamburger on:click={toggle} />
+    <NavUl {hidden}>
+      <NavLi href="/creators">
+        <div class="text-orange-500">
+            Creators
+        </div>
+      </NavLi>
+      <NavLi href="/careers">
+        <div class="text-orange-500">
+            Careers
+        </div>
+      </NavLi>
+      <NavLi href="/investors">
+        <div class="text-orange-500">
+            Investors            
+        </div>
+    </NavLi>
+      <NavLi href="/about-us">
+        <div class="text-orange-500">       
+            About Us
+        </div>
+       </NavLi>
+    </NavUl>
+  </Navbar>  
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { Alert } from 'flowbite-svelte';
+</script>
+
+<div class="p-8">
+
+</div>
