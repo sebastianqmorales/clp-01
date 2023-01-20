0609d505e7da48a1bef5b502c9ddbebe41295420
Sebastian <swebermorales@gmail.com>
2023-01-20 15:58:38 -0800

    adding more content to new landing page

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,37 +1,25 @@
 <script>
-  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte'
-  import Logo from './CuatexLogo.svg';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import Logo from './CuatexLogo.svg';
 </script>
 
 <Navbar let:hidden let:toggle>
-    <NavBrand href="/">
-      <img
-        src={Logo}
-        class="mr-3 h-6 sm:h-14"
-        alt="Cuatex Logo"
-      />
-    </NavBrand>
-    <NavHamburger on:click={toggle} />
-    <NavUl {hidden}>
-      <NavLi href="/creators">
-        <div class="text-orange-500 text-xl font-medium">
-            Creators 
-        </div>
-      </NavLi>
-      <NavLi href="/careers">
-        <div class="text-orange-500 text-xl font-medium">
-            Careers
-        </div>
-      </NavLi>
-      <NavLi href="/investors">
-        <div class="text-orange-500 text-xl font-medium">
-            Investors            
-        </div>
-    </NavLi>
-      <NavLi href="/about-us">
-        <div class="text-orange-500 text-xl font-medium">       
-            About Us
-        </div>
-       </NavLi>
-    </NavUl>
-  </Navbar>  
+	<NavBrand href="/">
+		<img src={Logo} class="mr-3 h-12 sm:h-14" alt="Cuatex Logo" />
+	</NavBrand>
+	<NavHamburger on:click={toggle} />
+	<NavUl {hidden}>
+		<NavLi href="/creators">
+			<div class="text-orange-500 text-xl font-medium">Creators</div>
+		</NavLi>
+		<NavLi href="/careers">
+			<div class="text-orange-500 text-xl font-medium">Careers</div>
+		</NavLi>
+		<NavLi href="/investors">
+			<div class="text-orange-500 text-xl font-medium">Investors</div>
+		</NavLi>
+		<NavLi href="/about-us">
+			<div class="text-orange-500 text-xl font-medium">About Us</div>
+		</NavLi>
+	</NavUl>
+</Navbar>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,102 +1,85 @@
 <script>
-	
 </script>
-
 
 <!-- Top section -->
 <div class="h-screen w-auto bg-orange-400 flex flex-col md:pt-20 md:gap-12 md:px-40">
-        <div class="text-5xl font-bold">Social media by users for users.</div>
-        <div class="flex  gap-10">
-        <div class="text-2xl">Social Media</div>
-        <div class="flex flex-col">
-            <div class="text-2xl ">that respects users privacy</div>
-            <div class="text-2xl ">that discourages misinformation</div>
-            <div class="text-2xl">that supports data digniry</div>
-        </div>
-    </div>
-    <div class="flex gap-10">
-        <div class="bg-custom p-3 text-2xl font-medium cursor-pointer shadow-2xl hover:shadow-none">
-                Learn More
-        </div>
-        <div class="bg-custom p-3 text-2xl font-medium cursor-pointer shadow-2xl hover:shadow-none">
-                Get notified of launch
-        </div>
-    </div>
+	<div class="text-5xl font-bold">Social media by users for users.</div>
+	<div class="flex  gap-10">
+		<div class="text-2xl">Social Media</div>
+		<div class="flex flex-col">
+			<div class="text-2xl ">that respects users privacy</div>
+			<div class="text-2xl ">that discourages misinformation</div>
+			<div class="text-2xl">that supports data digniry</div>
+		</div>
+	</div>
+	<div class="flex gap-10">
+		<div class="bg-custom p-3 text-2xl font-medium cursor-pointer shadow-2xl hover:shadow-none">
+			Learn More
+		</div>
+		<div class="bg-custom p-3 text-2xl font-medium cursor-pointer shadow-2xl hover:shadow-none">
+			Get notified of launch
+		</div>
+	</div>
 </div>
-    
+
 <!-- Second section -->
 <!-- <div class="bg-white h-screen w-auto flex flex-col md:pt-20 md:gap-12 md:px-40"> -->
-<div class="h-screen w-auto bg-gradient-to-b from-orange-400 to-transparent py-10 m-20 flex flex-col gap-10">
-    <div class="text-5xl text-center font-bold">What's the problem?</div>
-    <div class="text-2xl text-center">The problem is stuff being free, ever hear the saying, nothing is free?</div>
-    <div class="text-2xl font-bold text-center">How it should be.</div>
-    <div class="flex justify-evenly">
-        <div class="text-2xl">
-        Creator     
-        </div>
-        <div class="text-2xl">
-            Content
-        </div>
-        <div class="text-2xl">
-            Viewer
-        </div>
-    </div>
-    <div class="text-2xl font-bold text-center">How it is</div>
-    <div class="flex justify-evenly">
-        <div class="text-2xl ">
-            Platform     
-        </div>
-        <div class="text-2xl ">
-            User data
-        </div>
-        <div class="text-2xl">
-            Companies that pay for advertisments.
-        </div>
-    </div>
-    <div class="text-2xl font-bold text-center">How we want it to be</div>
-    <div class="flex justify-evenly">
-        <div class="text-2xl">
-            Creator     
-        </div>
-        <div class="text-2xl">
-            Content
-        </div>
-        <div class="text-2xl">
-            Viewer
-        </div>
-    </div>
+<div class=" w-auto bg-gradient-to-b from-orange-400 to-transparent p-10 m-20 flex flex-col gap-10">
+	<div class="text-5xl text-center font-bold">What's the problem?</div>
+	<div class="text-2xl text-left font-normal tracking-wide">
+		Platforms like Twitter, Instagram, YouTube, and TikTok have too much power.
+	</div>
+	<div class="text-2xl text-left font-normal tracking-wide">
+		They have complete control over the monetization of content, including: how many ads we have to
+		watch what content gets promoted or hidden the cut of the profits that goes to content creators
+	</div>
+	<div class="text-2xl text-left font-normal tracking-wide">
+		The companies only want money, so they incentivize creating content that gets the most ad
+		views—clickbait garbage that users don't actually enjoy because the platforms just want us to
+		scroll or click the next video, and watch another ad.
+	</div>
+	<div class="text-2xl text-left font-normal tracking-wide">
+		We're sick of it. So we're giving the power back to the users, because we love the creators and
+		hate the ads.
+	</div>
 </div>
 <!-- </div> -->
 
 <!-- Third section -->
-<div class="h-screen w-auto bg-gradient-to-t from-orange-400 to-transparent py-10 m-20 flex flex-col gap-10">
-    <div class="text-5xl font-bold text-center">How will Cuatex solve the problem?</div>
-    <div class="text-2xl">Our vision</div>
-    <div class="text-2xl">What does Cuatex offer you?</div>
-    <div class="flex gap-10">
-        <div class="container">
-            <div class="text-3xl">Viewer</div>
-            <ul>
-                <div class="text-2xl">No ads! You pay for content for as low as a fraction of a cent.</div>
-                <div class="text-2xl">Have nuanced conversations with Venn UI.</div>
-                <div class="text-2xl">Own and sell your data for a profit!</div>
-                <div class="text-2xl">You have the power! Vote, comment, tag and censor all content.</div>
-            </ul>
-        </div>
-        <div class="container">
-            <div class="text-3xl">Creator</div>
-            <ul>
-                <div class="text-2xl">Get paid for your content! Finally focus soley on value instead of sponsors.</div>
-                <div class="text-2xl">Understand your audience and further develop unique conent with Venn UI.</div>
-                <div class="text-2xl">Own and sell your data for a profit!</div>
-                <div class="text-2xl">Respond to your audience quickly and accuratley with detailed voting.</div>
-            </ul>
-        </div>
-    </div>
+<div class=" w-auto bg-gradient-to-t from-orange-400 to-transparent p-10 m-20 flex flex-col gap-10">
+	<div class="text-5xl font-bold text-center">How will Cuatex solve the problem?</div>
+	<div class="flex gap-10">
+		<div class="container">
+			<div class="text-2xl font-normal tracking-wide">No ads</div>
+			<ul>
+				<div class="text-2xl">Pay as little as a fraction of a cent to view content</div>
+			</ul>
+		</div>
+		<div class="container">
+			<div class="text-2xl font-normal tracking-wide">Transparent algorithms</div>
+			<ul>
+				<div class="text-2xl">User controlled feeds & algorithms</div>
+			</ul>
+		</div>
+	</div>
+	<div class="flex gap-10">
+		<div class="container">
+			<div class="text-2xl font-normal tracking-wide">Opt-in data collection</div>
+			<ul>
+				<div class="text-2xl">Get compensated for sharing your data, or don't</div>
+			</ul>
+		</div>
+		<div class="container">
+			<div class="text-2xl font-normal tracking-wide">More perspectives</div>
+			<ul>
+				<div class="text-2xl">Unique Venn UI encourages every perspective</div>
+			</ul>
+		</div>
+	</div>
 </div>
 
 <style>
-    .bg-custom{
-        background-color: #ff4d00;
-    }
+	.bg-custom {
+		background-color: #ff4d00;
+	}
 </style>
